26e2215dc933970ee7a8abb6e2050da4faa167d0
Sean Clifford <sclifford6262@gmail.com>
2018-05-05 16:37:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function lowerCaseDrivers(drivers){
   return drivers.map(function(driver){return driver.toLowerCase()})
 }
+
+function nameToAttributes(drivers){
+  return drivers.map(function(driver){return {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}})
+}
