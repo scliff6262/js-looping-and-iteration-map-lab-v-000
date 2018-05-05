65b40c0182286bfe41b749f9f1359a5f24d524b6
Sean Clifford <sclifford6262@gmail.com>
2018-05-05 16:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function lowerCaseDrivers(drivers){
 function nameToAttributes(drivers){
   return drivers.map(function(driver){return {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}})
 }
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(driver){return `${driver.name} is from ${driver.hometown}`})
+}
